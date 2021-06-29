e13ba75e898ca452e726e6176fce1aeae95d211b
AhDaii <1171469399@qq.com>
2021-06-29 20:02:43 +0800

    update

---
+++ CppPrimerPlus/Chapter_5/Review/Review_04.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int j = 5;
+    while(++ j < 9)
+        cout << j ++ << endl;
+    return 0;
+}
---
+++ CppPrimerPlus/Chapter_5/Review/Review_06.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 1;
+    while(count <= 64) {
+        cout << count << endl;
+        count *= 2;
+    }
+    return 0;
+}
